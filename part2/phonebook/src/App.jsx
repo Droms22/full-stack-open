a65ab2a65608845d3e2e204ed3c434cdfbd1efb6
Droms22 <domenicof2211@gmail.com>
2024-12-30 20:07:27 +0100

    Add exercise 2.11 + fix ID generation

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
 import Filter from './components/Filter';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [newPerson, setNewPerson] = useState({ name: '', number: '' });
-  const [nextId, setNextId] = useState(5);
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      });
+  }, []);
 
   const filteredPersons = persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()));
 
@@ -40,13 +42,13 @@ const App = () => {
       alert(`${newPerson.number} is already added to phonebook`);
     } else {
       //add new person
+      const maxId = Math.max(...persons.map(item => item.id));
+
       setPersons(persons.concat({
         name: newPerson.name,
         number: newPerson.number,
-        id: nextId
+        id: maxId + 1
       }));
-
-      setNextId(nextId + 1);
     }
 
     //reset form
